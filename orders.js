de5385ce37016e534d4e535fb9bc5f99d5ce77d5
ACTR1 <120684616+ACTR1@users.noreply.github.com>
2023-08-10 22:52:18 -0300

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,45 @@
+const Orders = [
+    {
+        productName: 'Foldable mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName:'LARVENDER KF102 DRONE' ,
+        productNumber:'36378' ,
+        paymentStatus: 'REFUNDED',
+        shipping:'DECLINED' 
+    },
+    {
+        productName:'ruko f11 pro Drone' ,
+        productNumber:'49347' ,
+        paymentStatus: 'Due',
+        shipping:'Pending' 
+    },
+    {
+        productName:'Drone with camera drone' ,
+        productNumber:'96996' ,
+        paymentStatus: 'Paid',
+        shipping:'Delivered' 
+    },
+    {
+        productName:'GPS 4K DRONE' ,
+        productNumber:'22821' ,
+        paymentStatus: 'Paid',
+        shipping:'Delivered' 
+    },
+    {
+        productName:'DJI Air 2s' ,
+        productNumber:'81475' ,
+        paymentStatus: 'Due',
+        shipping:'Pending' 
+    },
+    {
+        productName:'lozenge Drone' ,
+        productNumber:'00482' ,
+        paymentStatus: 'Paid',
+        shipping:'Delivered' 
+    }
+
+]
